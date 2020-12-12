35a51b7ef1f7232a147cc291a64b4fa5a514f09d
spare15 <66483573+spare15@users.noreply.github.com>
2020-12-13 05:38:20 +0900

    Update address.c

--- code/ch08/address.c
+++ code/ch08/address.c
@@ -1,1 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main(void)
+{
+	int input;
+
+	printf("정수 입력: ");
+	scanf("%d", &input);
+	printf("입력삾: %d\n", input);
+	printf("주소값: %u(10진수), %p(16진수)\n", (int)&input, &input);
+	printf("주소값: %d(10진수), %#x(16진수)\n", (unsigned) &input, (int) &input);
+	printf("주소값 크기: %d\n", sizeof(&input));
+
+	return 0;
+}
